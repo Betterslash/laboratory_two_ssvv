7523dc6c1c186c28aca133123d582d897de40523
Betterslash <dory7141@gmail.com>
2022-03-28 21:22:52 +0300

    refacotring 0.7

--- src/main/java/validation/StudentValidator.java
+++ src/main/java/validation/StudentValidator.java
@@ -2,6 +2,8 @@ package validation;
 
 import domain.Student;
 
+import java.util.Objects;
+
 public class StudentValidator implements Validator<Student> {
 
     /**
@@ -17,7 +19,8 @@ public class StudentValidator implements Validator<Student> {
         if(entity.getID() == null){
             throw new ValidationException("Id incorect!");
         }
-        if(entity.getNume() == ""){
+        //TODO replaced with equals
+        if(Objects.equals(entity.getNume(), "")){
             throw new ValidationException("Nume incorect!");
         }
         if(entity.getGrupa() < 0) {
